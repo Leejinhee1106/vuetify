f90d7b946d4e046060300daa60dc8211954af76e
Leejinhee1106 <dlwlsgml1106@gmail.com>
2025-06-03 17:55:10 +0900

    Add files via upload
    0603 최종

--- src/components/coms/Tabs.vue
+++ src/components/coms/Tabs.vue
@@ -1,11 +1,51 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col cols="12">
-            </v-col>
-        </v-row>
-    </v-container>
+<v-container>
+    <v-row>
+        <v-col cols="12" md="4">
+            <v-card>
+                <v-tabs v-model="tab" bg-color="primary">
+                    <v-tab value="one">Item One</v-tab>
+                    <v-tab value="two">Item Two</v-tab>
+                    <v-tab value="three">Item Three</v-tab>
+                </v-tabs>
+            
+                <v-card-text>
+                    <v-tabs-window v-model="tab">
+                        <v-tabs-window-item value="one">
+                            one
+                        </v-tabs-window-item>
+                        <v-tabs-window-item value="two">
+                            two
+                        </v-tabs-window-item>
+                        <v-tabs-window-item value="three">
+                           three
+                        </v-tabs-window-item>
+                    </v-tabs-window>
+                </v-card-text>
+            </v-card>
+        </v-col>  
+
+        <v-col cols="12" md="4">
+            <v-card>
+                <v-tabs v-model="tab" align-tabs="center" color="deep-purple-accent-4">
+                    <v-tab :value="1">Landscape</v-tab>
+                    <v-tab :value="2">City</v-tab>
+                    <v-tab :value="3">Abstract</v-tab>
+                </v-tabs>
+            </v-card>
+        </v-col>
+
+        <v-col cols="12" md="4">
+        
+        </v-col>           
+    </v-row>
+</v-container>
 </template>
 
-<script setup>
+<script>
+export default {
+    data:()=> ({
+        tab: null,
+    }),
+}
 </script>
--- src/include/Header.vue
+++ src/include/Header.vue
@@ -8,14 +8,14 @@
             <v-btn text :to="'/nav3'" >게</v-btn>
             <v-btn text :to="'/nav4'" >이</v-btn>
             <v-btn text :to="'/nav5'" >션*</v-btn>
-            <v-btn text :to="'/bottom1'" >B_nav1</v-btn>
-            <v-btn text :to="'/footer'" >Footer</v-btn>
+            <v-btn text :to="'/tabs'">Tabs</v-btn>
             <v-btn text :to="'/button'" >button</v-btn>
             <v-btn text :to="'/fbutton'" >Floating Btn</v-btn>
             <v-btn text :to="'/expan'" >Expan</v-btn>
             <v-btn text :to="'/tooltip'">Tooltip</v-btn>
-            <v-btn text :to="'/breadcrumbs'">Crumbles</v-btn>
-            <v-btn text :to="'/tabs'">Tabs</v-btn>
+            <v-btn text :to="'/breadcrumbs'">Crumbles</v-btn> 
+            <v-btn text :to="'/bottom1'" >B_nav1</v-btn>
+            <v-btn text :to="'/footer'" >Footer</v-btn>          
 </v-app-bar>
 
             <v-main>
