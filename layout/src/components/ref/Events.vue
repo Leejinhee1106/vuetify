285565c773685b640a9d4d418a4c36922a5f9e25
Leejinhee1106 <dlwlsgml1106@gmail.com>
2025-06-09 17:25:10 +0900

    Update Events.vue
    final

--- layout/src/components/ref/Events.vue
+++ layout/src/components/ref/Events.vue
@@ -6,8 +6,26 @@
         </p>
         <hr class="my-4"/>
         <p>{{ "count" + count }}</p>
-        <button v-on:click="count++">Count 증가</button>
-        <v-btn v-on:click="count++" variant="primary">Count 증가</v-btn>
+        <button v-on:click="count--">Count 감소</button>
+        <v-btn v-on:click="count++" color="primary" variant="elevated">Count 증가</v-btn>
+        <hr class="my-4"/>
+        <v-row>
+            <v-col cols="12" md="4">
+                <div id="lightDiv">
+                    <div v-show="lightOn"></div>
+                    <img src="../../assets/img_lightBulb.svg">
+                </div>
+                <v-btn v-on:click="lightOn = !lightOn">Switch on/off</v-btn>
+            </v-col>
+
+            <v-col cols="12" md="4">
+            
+            </v-col>
+
+            <v-col cols="12" md="4">
+            
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
@@ -15,8 +33,32 @@
 export default {
     data(){
         return{
-            count:0
+            count:0,
+            lightOn: false,
+
         }
     }
 }
 </script>
+
+<style>
+#lightDiv{
+    position: relative;
+    width: 150px;
+    height: 150px;
+}
+#lightDiv > img{
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+#lightDiv > div{
+    position: absolute;
+    top: 10%;
+    left: 10%;
+    width: 80%;
+    height: 80%;
+    border-radius: 50%;
+    background: yellow;
+}
+</style>
